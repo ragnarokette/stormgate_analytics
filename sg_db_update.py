7229120eef7c6eb2fac80fc1470765e47d350dd2
Maeve Cotton <ragnarokette@gmail.com>
2024-02-11 02:32:28 -0500

    script to add results to the database

--- sg_db_update.py
+++ sg_db_update.py
@@ -1,8 +1,23 @@
 import csv
 import sqlite3
+import pandas as pd
 
-con = sqlite3.connect("C:\Program Files\SQLiteStudio\Stormgate")
+con = sqlite3.connect("StormgateDB")
 
 cur = con.cursor()
 
-cur.execute("drop table if exists players")
+#update players table
+cur.execute("delete from players;")
+players_df = pd.read_csv('results/players_20240209_2006.csv')
+players_df.to_sql('players', con, if_exists='append', index=False)
+
+#update matches table
+matches_df = pd.read_csv('results/matches_20240209_1918.csv')
+matches_df.to_sql('matches', con, if_exists='append', index=False)
+
+#update player_matches table
+pm_df = pd.read_csv('results/player_matches_20240209_1918.csv')
+pm_df.to_sql('player_matches', con, if_exists='append', index=False)
+
+con.commit()
+con.close()
